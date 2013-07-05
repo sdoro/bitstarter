af6c6a27e1b33cb233114eba29ff246c548e4aef
Sandro Doro <sandro.doro@gmail.com>
2013-07-05 10:17:40 +0000

    Testing HW3 part1

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(16);
+buffer.realFileSync("index.html");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    responde.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
