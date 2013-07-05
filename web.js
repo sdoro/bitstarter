6264bf194e5d7aa447cbfe8794a733106c059b5a
Sandro Doro <sandro.doro@gmail.com>
2013-07-05 10:20:22 +0000

    Fixed a typo

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var buffer = new Buffer(16);
-buffer.realFileSync("index.html");
+buffer.readFileSync("index.html");
 
 app.get('/', function(request, response) {
     responde.send(buffer.toString());
